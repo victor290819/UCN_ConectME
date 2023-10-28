b299c20979f696bd4d352b4b5e3bca5b457bdf4a
bosco <aboscoaburto1234@gmail.com>
2023-10-28 01:52:24 -0600

    Pcamr

---
+++ app/src/main/java/com/example/ucn_conectme/BuscarAmigosActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ucn_conectme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BuscarAmigosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buscar_amigos)
+    }
+}
